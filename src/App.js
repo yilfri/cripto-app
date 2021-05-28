f26e90e8cd36cb6d83025f13a43581a1cdb95dbe
Yilfri Salave <yilfri97@hotmail.com>
2021-05-27 21:19:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from '@emotion/styled';
+import image from './cripto.jpg';
+
+const Contenedor = styled.div`
+	max-width: 900px;
+	margin: 0 auto;
+	@media (min-width: 992px) {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		column-gap: 2rem;
+	}
+`;
+
+const Image = styled.img`
+	max-width: 100%;
+	margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+	font-family: 'Bebas Neue', cursive;
+	color: #fff;
+	text-align: left;
+	font-weight: 700;
+	font-size: 50px;
+	margin-bottom: 50px;
+	margin-top: 80px;
+
+	&&::after {
+		content: '';
+		width: 100px;
+		height: 6px;
+		background-color: #66a2fe;
+		display: block;
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Contenedor>
+			<div>{<Image src={image} alt="Cryptocurrency" />}</div>
+			<div>
+				<Heading>Cotiza Criptomonedas al Instange</Heading>
+			</div>
+		</Contenedor>
+	);
 }
 
 export default App;
